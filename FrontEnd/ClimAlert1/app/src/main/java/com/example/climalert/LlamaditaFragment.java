6474a5560f9bf6eec06180296dd93316beac6749
davidrevillac <david.revilla@estudiantat.upc.edu>
2021-12-02 20:08:32 +0100

    ya funciona ubi y llamdita

--- FrontEnd/ClimAlert1/app/src/main/java/com/example/climalert/LlamaditaFragment.java
+++ FrontEnd/ClimAlert1/app/src/main/java/com/example/climalert/LlamaditaFragment.java
@@ -1,9 +1,15 @@
 package com.example.climalert;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +18,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.climalert.CosasDeTeo.InformacionUsuario;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,6 +54,11 @@ public class LlamaditaFragment extends Fragment {
     private Spinner mSpinner;
     EditText descripcion;
     Button aceptar;
+    Button SOS;
+    private String nombrefen = "Incendio";
+    String date;
+    String hour;
+    String spinnerres;
 
     public LlamaditaFragment() {
         // Required empty public constructor
@@ -83,21 +110,83 @@ public class LlamaditaFragment extends Fragment {
         incidencias.add("Gota Fría");
         incidencias.add("Tormenta Eléctrica");
 
-
+        date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        Log.d("prueba", date);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        hour = sdf.format(new Date());
+        Log.d("prueba", hour);
         ArrayAdapter adp;
         adp = new ArrayAdapter(getActivity(),
                 android.R.layout.simple_spinner_dropdown_item,
                 incidencias);
         mSpinner.setAdapter(adp);
+        //on click spinner con variable
         descripcion = (EditText) view.findViewById(R.id.editDescripcion);
         aceptar = (Button) view.findViewById(R.id.btnAceptar);
+        SOS = (Button) view.findViewById(R.id.btnSOS);
+
+        SOS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_CALL, Uri.parse("tel://añadir telefono al que llamar"));
+                if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                    return;
+                startActivity(i);
+            }
+        });
 
         aceptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Log.d("ubi", String.valueOf(InformacionUsuario.getInstance().latitudactual));
+                Log.d("ubi", String.valueOf(InformacionUsuario.getInstance().longitudactual));
+                //dar_incidencia();
             }
         });
         return view;
+    }
+
+    public void dar_incidencia() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        String url = "https://climalert.herokuapp.com/incidencia/new";
+        JSONObject mapa = new JSONObject();
+        try {
+            spinnerres = mSpinner.getSelectedItem().toString();
+            mapa.put("nombreFenomeno", spinnerres);
+            if (InformacionUsuario.getInstance().latitudactual != 0) {
+                mapa.put("latitud", String.valueOf(InformacionUsuario.getInstance().latitudactual));
+                mapa.put("longitud", String.valueOf(InformacionUsuario.getInstance().longitudactual));
+            }
+            date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+            mapa.put("fecha", date);
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+            hour = sdf.format(new Date());
+            mapa.put("hora", hour);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, mapa,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //JSONObject usuario;
+                        Log.d("a", String.valueOf(response));
+                        //Log.d("ALGO", "he acabado el bucle");
+
+                        //ir a mapa
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+
+                }) {
+        };
+        queue.add(request);
     }
 }
