c730acf369d61c01b027c241164a6bf09da19eca
WanderingPhantom <oriol.cardona.alsina@gmail.com>
2021-12-18 17:00:39 +0100

    Añadir codigo para los botones de admin
    Los botones de gestion de admin estan listos para ser programados

--- FrontEnd/ClimAlert1/app/src/main/java/com/example/climalert/VentanaAdminFragment.java
+++ FrontEnd/ClimAlert1/app/src/main/java/com/example/climalert/VentanaAdminFragment.java
@@ -12,7 +12,7 @@ import androidx.fragment.app.Fragment;
 import com.example.climalert.CosasDeTeo.InformacionUsuario;
 import com.google.android.material.slider.Slider;
 
-public class VentanaAdminFragment extends Fragment {
+public class VentanaAdminFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +23,27 @@ public class VentanaAdminFragment extends Fragment {
         // Inflate the layout for this fragment
         //Todos estos botones y cosas se pueden hacer en local
         View view = inflater.inflate(R.layout.fragment_admin, container, false);
-
+        Button gestionar_refugios = (Button) view.findViewById(R.id.gestionar_refugios_button);
+        gestionar_refugios.setOnClickListener(this);
+        Button gestionar_usuarios = (Button) view.findViewById(R.id.gestionar_usuarios_button);
+        gestionar_usuarios.setOnClickListener(this);
+        Button validar_incidencias = (Button) view.findViewById(R.id.validar_incidencias_button);
+        validar_incidencias.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.gestionar_refugios_button:
+                break;
+                
+            case R.id.gestionar_usuarios_button:
+                break;
+
+            case R.id.validar_incidencias_button:
+                break;
+        }
+
     }
 }
