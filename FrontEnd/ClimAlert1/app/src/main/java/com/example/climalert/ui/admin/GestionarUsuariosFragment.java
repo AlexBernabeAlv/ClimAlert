a15dd276f25d03e3330612facac9e812d9281c6b
WanderingPhantom <oriol.cardona.alsina@gmail.com>
2021-12-22 18:03:52 +0100

    botones dinamicos
    Falta que muestre el email

--- FrontEnd/ClimAlert1/app/src/main/java/com/example/climalert/ui/admin/GestionarUsuariosFragment.java
+++ FrontEnd/ClimAlert1/app/src/main/java/com/example/climalert/ui/admin/GestionarUsuariosFragment.java
@@ -1,11 +1,19 @@
 package com.example.climalert.ui.admin;
 
+import static java.lang.Thread.sleep;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import androidx.fragment.app.Fragment;
 
@@ -19,6 +27,9 @@ import com.example.climalert.CosasDeTeo.InformacionUsuario;
 import com.example.climalert.CosasDeTeo.Notificacion;
 import com.example.climalert.CosasDeTeo.UsuarioEstandar;
 import com.example.climalert.R;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,6 +39,7 @@ import java.util.Vector;
 
 public class GestionarUsuariosFragment extends Fragment {
     JSONObject mapa = new JSONObject();
+    LinearLayout linearLayout;
     public Vector<UsuarioEstandar> usuariosEstandar = new Vector<UsuarioEstandar>();
 
     @Override
@@ -39,8 +51,63 @@ public class GestionarUsuariosFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         getUsuariosEstandar();
+        try {
+            sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int n = usuariosEstandar.capacity();
         View view = inflater.inflate(R.layout.fragment_usuarios, container, false);
+        linearLayout = view.findViewById(R.id.linear_layout_usuarios);
+        for(int i = 1; i <= n; ++i) {
+            Button btn = new Button(getContext());
+            btn.setLayoutParams(new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            btn.setId(i);
+            btn.setText(String.valueOf(i)); //esto ha de ser el email del usuario
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Button ban = new Button(getContext());
+                    ban.setLayoutParams(new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+                    ban.setText("BAN");
+                    ban.setBackgroundColor(Color.RED);
+                    linearLayout.addView(ban);
+                    ban.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            banear();
+                        }
+                    });
+                    Button ver = new Button(getContext());
+                    ver.setLayoutParams(new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT));
+                    ver.setText("VER");
+                    ver.setBackgroundColor(Color.GREEN);
+                    linearLayout.addView(ver);
+                    ver.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ver_perfil();
+                        }
+                    });
+
+                }
+            });
+            linearLayout.addView(btn);
+        }
         return view;
+
+    }
+
+    private void banear() {
+    }
+
+    private void ver_perfil(){
 
     }
 
@@ -92,4 +159,23 @@ public class GestionarUsuariosFragment extends Fragment {
         };
         queue.add(request);
     }
+
+    private void buclear() {
+
+        refresh(600);
+    }
+
+    private void refresh(int milliseconds){
+        final Handler handler = new Handler();
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                buclear();
+            }
+        };
+        handler.postDelayed(runnable, milliseconds);
+
+    }
+
 }
